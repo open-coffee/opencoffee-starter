d4a23d4754df5409a866a46bb4238d983cd98499
Tobias Schneider <schneider@synyx.de>
2017-01-12 16:18:01 +0100

    Add method to check if a CoffeeNetUserDetails has certain roles

--- coffeenet-autoconfigure/src/main/java/coffee/synyx/autoconfigure/security/user/CoffeeNetUserDetails.java
+++ coffeenet-autoconfigure/src/main/java/coffee/synyx/autoconfigure/security/user/CoffeeNetUserDetails.java
@@ -1,8 +1,11 @@
 package coffee.synyx.autoconfigure.security.user;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+
+import org.springframework.util.Assert;
 
 import java.util.Collection;
 import java.util.Set;
@@ -13,6 +16,7 @@ import static java.util.stream.Collectors.toSet;
 /**
  * Extension of {@link UserDetails} is required for {@link OAuth2Authentication#getName()} to work.
  *
+ * @author  Tobias Schneider - schneider@synyx.de
  * @author  David Schilling - schilling@synyx.de
  */
 public interface CoffeeNetUserDetails extends UserDetails {
@@ -26,6 +30,21 @@ public interface CoffeeNetUserDetails extends UserDetails {
 
     @Override
     Collection<? extends GrantedAuthority> getAuthorities();
+
+
+    default boolean hasRoles(String... roles) {
+
+        for (String role : roles) {
+            Assert.isTrue(!role.startsWith("ROLE_"), role
+                + " cannot start with ROLE_ (it is automatically added)");
+
+            if (!getAuthorities().contains(new SimpleGrantedAuthority("ROLE_" + role))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
 
     boolean isCoffeeNetAdmin();
--- coffeenet-autoconfigure/src/test/java/coffee/synyx/autoconfigure/security/user/CoffeeNetUserDetailsTest.java
+++ coffeenet-autoconfigure/src/test/java/coffee/synyx/autoconfigure/security/user/CoffeeNetUserDetailsTest.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import static org.hamcrest.Matchers.hasItems;
 
@@ -33,10 +34,59 @@ public class CoffeeNetUserDetailsTest {
 
 
     @Test
+    public void hasRoles() {
+
+        CoffeeNetUserDetails coffeeNetUserDetails = new CoffeeNetUserDetailsStub();
+        assertThat(coffeeNetUserDetails.hasRoles("COFFEENET-ADMIN", "COFFEENET-USER"), is(true));
+    }
+
+
+    @Test
+    public void hasRolesNotAll() {
+
+        CoffeeNetUserDetails coffeeNetUserDetails = new CoffeeNetUserDetailsStub();
+        assertThat(coffeeNetUserDetails.hasRoles("COFFEENET-ADMIN", "COFFEENET-WRONG-USER"), is(false));
+    }
+
+
+    @Test
+    public void hasRolesNot() {
+
+        CoffeeNetUserDetails coffeeNetUserDetails = new CoffeeNetUserDetailsStub();
+        assertThat(coffeeNetUserDetails.hasRoles("COFFEENET-WRONG-USER"), is(false));
+    }
+
+
+    @Test
+    public void hasRolesNotAndEmptyRole() {
+
+        CoffeeNetUserDetails coffeeNetUserDetails = new CoffeeNetUserDetailsStub();
+        assertThat(coffeeNetUserDetails.hasRoles(), is(true));
+    }
+
+
+    @Test
+    public void hasRolesProvidesEmpty() {
+
+        CoffeeNetUserDetails coffeeNetUserDetails = new CoffeeNetUserDetailsStub();
+        assertThat(coffeeNetUserDetails.hasRoles(), is(true));
+    }
+
+
+    @Test
+    public void hasRolesHasEmpty() {
+
+        CoffeeNetUserDetails coffeeNetUserDetails = new CoffeeNetUserDetailsEmptyStub();
+        assertThat(coffeeNetUserDetails.hasRoles("COFFEENET-USER"), is(false));
+    }
+
+
+    @Test
     public void getAuthoritiesAsString() {
 
         CoffeeNetUserDetails coffeeNetUserDetails = new CoffeeNetUserDetailsStub();
-        assertThat(coffeeNetUserDetails.getAuthoritiesAsString(), is(hasItems("COFFEENET-ADMIN", "COFFEENET-USER")));
+        assertThat(coffeeNetUserDetails.getAuthoritiesAsString(),
+            is(hasItems("ROLE_COFFEENET-ADMIN", "ROLE_COFFEENET-USER")));
     }
 
     private class CoffeeNetUserDetailsStub implements CoffeeNetUserDetails {
@@ -58,7 +108,45 @@ public class CoffeeNetUserDetailsTest {
         @Override
         public Collection<? extends GrantedAuthority> getAuthorities() {
 
-            return asList(new SimpleGrantedAuthority("COFFEENET-ADMIN"), new SimpleGrantedAuthority("COFFEENET-USER"));
+            return asList(new SimpleGrantedAuthority("ROLE_COFFEENET-ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_COFFEENET-USER"));
+        }
+
+
+        @Override
+        public boolean isCoffeeNetAdmin() {
+
+            return true;
+        }
+
+
+        @Override
+        public boolean isMachineUser() {
+
+            return false;
+        }
+    }
+
+    private class CoffeeNetUserDetailsEmptyStub implements CoffeeNetUserDetails {
+
+        @Override
+        public String getUsername() {
+
+            return null;
+        }
+
+
+        @Override
+        public String getEmail() {
+
+            return null;
+        }
+
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+
+            return Collections.emptyList();
         }
 
 
