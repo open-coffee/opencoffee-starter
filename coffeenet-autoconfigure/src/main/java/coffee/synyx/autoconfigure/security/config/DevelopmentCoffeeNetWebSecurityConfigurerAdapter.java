709015f61a707be60de31dfd98720a82c2283b2a
Tobias Schneider <tobias.schneider@tobsch.org>
2017-01-26 19:58:44 +0100

    Remove * in javadoc

--- coffeenet-autoconfigure/src/main/java/coffee/synyx/autoconfigure/security/config/DevelopmentCoffeeNetWebSecurityConfigurerAdapter.java
+++ coffeenet-autoconfigure/src/main/java/coffee/synyx/autoconfigure/security/config/DevelopmentCoffeeNetWebSecurityConfigurerAdapter.java
@@ -15,7 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * <p>By default two user will be provided:</p>
  *
  * <ul>
- * <li>admin with the password admin and the role 'COFFEENET-ADMIN'</li>*
+ * <li>admin with the password admin and the role 'COFFEENET-ADMIN'</li>
  * <li>user with the password user without a role</li>
  * </ul>
  *
