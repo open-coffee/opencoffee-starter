810d0ada59cb65778b3c61a478b9c2c69e793e80
Tobias Schneider <tobias.schneider@tobsch.org>
2017-08-30 20:30:40 +0200

    Improve automatic script for gitHub

--- .scripts/automatic-coffeenet-parent-updater.sh
+++ .scripts/automatic-coffeenet-parent-updater.sh
@@ -5,6 +5,10 @@ if [ 1 -eq $# ]; then
 
   # global
   token=$1
+  gitUserName="CoffeeNet Release Dude"
+  gitUserEmail="coffeenet@Tobsch.org"
+
+  starterParentArtifactId="coffeenet-starter-parent"
   organizationFrom="coffeenetrelease"
   branch="update-to-latest-coffeenet-parent-version"
   projectsDir="/tmp/merge-requests"
@@ -14,117 +18,138 @@ if [ 1 -eq $# ]; then
   for projectInformation in "${projects[@]}"
   do
 
-    project=$(cut -d "/" -f 2 <<< "$projectInformation")
-    organizationTo=$(cut -d "/" -f 1 <<< "$projectInformation")
+    project=$(cut -d "/" -f 2 <<< "${projectInformation}")
+    organizationTo=$(cut -d "/" -f 1 <<< "${projectInformation}")
 
-    echo "# $project"
+    echo -e "\n\n# --------------------------------"
+    echo "# ${projectInformation}"
     echo -e "# --------------------------------\n"
 
-    echo "> Trying to fork $organizationTo/$project"
+    echo "> Trying to fork ${organizationTo}/${project}"
     forkHttpStatus=$(/usr/bin/curl \
+      --location \
+      --post301 \
       --write-out '%{http_code}' \
       --silent \
       --output /dev/null \
       -X POST \
-      -H "Authorization: token $token" \
+      -H "Authorization: token ${token}" \
       -H "Accept: application/vnd.github.v3+json" \
-      "https://api.github.com/repos/$organizationTo/$project/forks")
+      "https://api.github.com/repos/${organizationTo}/${project}/forks")
     if [[ "$forkHttpStatus" == "202" ]]; then
-      echo -e "\n> Forked $organizationTo/$project\n"
+      echo -e "  -> Forked ${organizationTo}/${project}\n"
     else
-      echo -e "\n> Could not fork $organizationTo/$project ($forkHttpStatus) -> exiting"
-      exit -1
+      echo -e "  -> Could not fork ${organizationTo}/${project} ($forkHttpStatus)"
+      continue
     fi
 
     if [[ ! -e ${projectsDir} ]]; then
       mkdir -p ${projectsDir}
-      echo -e "> Created $projectsDir\n"
+      echo -e "> Created ${projectsDir}\n"
     fi
 
     if [[ ! -e ${projectsDir}/${project} ]]; then
-      /usr/bin/git clone https://github.com/${organizationFrom}/${project} ${projectsDir}/${project}
-      echo -e "> Cloned 'https://github.com/$organizationFrom/$project' to '$projectsDir/$project'\n"
+      /usr/bin/git clone https://github.com/${organizationFrom}/${project} ${projectsDir}/${project} &> /dev/null
+      echo -e "> Cloned 'https://github.com/${organizationFrom}/${project}' to '${project}sDir/${project}'\n"
+    fi
+
+    /usr/bin/git -C ${projectsDir}/${project} config user.name "${gitUserName}" &> /dev/null
+    /usr/bin/git -C ${projectsDir}/${project} config user.email "${gitUserEmail}" &> /dev/null
+    echo -e "> Set git user to '${gitUserName}' with email '${gitUserEmail}'\n"
+
+    if [ ! -f ${projectsDir}/${project}/pom.xml ]; then
+        echo -e ">>>> Skipping '${projectInformation}', because it is not a maven project\n"
+        continue
     fi
 
     if ! /usr/bin/git -C ${projectsDir}/${project} ls-remote coffeenet &>/dev/null; then
-      /usr/bin/git -C ${projectsDir}/${project} remote add coffeenet https://${token}@github.com/${organizationTo}/${project}.git
-     echo -e "> Add coffeenet remote ($organizationTo/$project)\n"
+      /usr/bin/git -C ${projectsDir}/${project} remote add coffeenet https://${token}@github.com/${organizationTo}/${project}.git &> /dev/null
+      echo -e "> Add coffeenet remote (${organizationTo}/${project})\n"
     fi
 
-    /usr/bin/git -C ${projectsDir}/${project} remote set-url origin https://${token}@github.com/${organizationFrom}/${project}.git
+    /usr/bin/git -C ${projectsDir}/${project} remote set-url origin https://${token}@github.com/${organizationFrom}/${project}.git &> /dev/null
     echo -e "> Changed the origin remote url with access token\n"
 
     CURRENT_BRANCH=$(/usr/bin/git -C ${projectsDir}/${project} rev-parse --abbrev-ref HEAD)
     if [[ "$CURRENT_BRANCH" != "master" ]]; then
-      /usr/bin/git -C ${projectsDir}/${project} checkout master
+      /usr/bin/git -C ${projectsDir}/${project} checkout master &> /dev/null
       echo -e "> Changed to master branch\n"
     fi
 
-    /usr/bin/git -C ${projectsDir}/${project} fetch -p
-    echo -e "> Removed local orphan branches\n"
-
-    /usr/bin/git -C ${projectsDir}/${project} pull --rebase coffeenet master
-    echo -e "> Rebased '$projectsDir/$project' to latest coffeenet master\n"
+    /usr/bin/git -C ${projectsDir}/${project} pull --rebase coffeenet master --prune &> /dev/null
+    echo -e "> Updated master branch and removed local orphan branches\n"
 
     if /usr/bin/git -C ${projectsDir}/${project} status; then
-      /usr/bin/git -C ${projectsDir}/${project} push origin master
+      /usr/bin/git -C ${projectsDir}/${project} push origin master &> /dev/null
       echo -e "> Updated ${organizationFrom}/${project} master with the master from ${organizationTo}/${project}\n"
     fi
 
+    parentArtifactId=`/usr/bin/mvn -f ${projectsDir}/${project}/pom.xml org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.parent.artifactId | grep -Ev '(^\[|Download\w+:)'`
+    if [ "${parentArtifactId}" != "${starterParentArtifactId}" ]; then
+        echo -e ">>>> Skipping '${projectInformation}', because not using '${starterParentArtifactId}' ('${parentArtifactId}')\n"
+        continue
+    fi
+
     # updating the parent version
-    /usr/bin/mvn -f ${projectsDir}/${project}/pom.xml versions:update-parent &>/dev/null
+    echo -e "> Trying to update the parent...\n"
+    /usr/bin/mvn -f ${projectsDir}/${project}/pom.xml versions:update-parent -q
 
     if /usr/bin/git -C ${projectsDir}/${project} status | grep -q pom.xml; then
 
-      coffeeNetParentVersion=`/usr/bin/mvn -f ${projectsDir}/${project}/pom.xml help:evaluate -Dexpression=project.parent.version | grep -e '^[^\[]'`
-      echo -e "> Updated CoffeeNet parent version of '$projectsDir/$project' to $coffeeNetParentVersion\n"
+      coffeeNetParentVersion=`/usr/bin/mvn -f ${projectsDir}/${project}/pom.xml org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.parent.version | grep -Ev '(^\[|Download\w+:)'`
+      currentParentVersion=${coffeeNetParentVersion// }
+      echo -e "> Updated CoffeeNet parent version of '${projectInformation}' to '${coffeeNetParentVersion}'\n"
+
+      if [ "${coffeeNetParentVersion}" == "@" ]; then
+          echo -e ">>>> Skipping '${projectInformation}', because wrong parent version '${coffeeNetParentVersion}'\n"
+          continue
+      fi
 
       updateBranch=${branch}-${coffeeNetParentVersion}
-      echo -e "> Branch for the pull request base is $updateBranch\n";
+      echo -e "> Branch for the pull request base is ${updateBranch}\n";
 
-      /usr/bin/git -C ${projectsDir}/${project} checkout -b ${updateBranch}
-      echo -e "> Changed branch to '$updateBranch'\n"
+      /usr/bin/git -C ${projectsDir}/${project} checkout -b ${updateBranch} &> /dev/null
+      echo -e "> Changed branch to '${updateBranch}'\n"
 
       rm ${projectsDir}/${project}/pom.xml.versionsBackup
-      echo -e "> Removed $projectsDir/$project/pom.xml.versionsBackup\n"
+      echo -e "> Removed ${project}sDir/${project}/pom.xml.versionsBackup\n"
 
-      /usr/bin/git -C ${projectsDir}/${project} commit -am "update to latest CoffeeNet parent version $coffeeNetParentVersion"
-      echo -e "> Created commit to update to latest CoffeeNet parent version $coffeeNetParentVersion\n"
+      /usr/bin/git -C ${projectsDir}/${project} commit -am "update to latest CoffeeNet parent version ${coffeeNetParentVersion}" &> /dev/null
+      echo -e "> Created commit to update to latest CoffeeNet parent version ${coffeeNetParentVersion}\n"
 
-      /usr/bin/git -C ${projectsDir}/${project} push -u origin ${updateBranch}
-      echo -e "> Pushed CoffeeNet parent update to $organizationFrom/$updateBranch\n"
+      /usr/bin/git -C ${projectsDir}/${project} push -u origin ${updateBranch} -f &> /dev/null
+      echo -e "> Pushed CoffeeNet parent update to ${organizationFrom}/${updateBranch}\n"
 
       pull_request_body="{
-        \"title\": \"Update to latest CoffeeNet parent version $coffeeNetParentVersion\",
-        \"body\": \"New CoffeeNet parent version $coffeeNetParentVersion was released! Please check
-          https://github.com/coffeenet/coffeenet-starter/blob/master/CHANGELOG.md for more information\",
-        \"head\": \"$organizationFrom:$updateBranch\",
+        \"title\": \"Update to latest CoffeeNet parent version ${coffeeNetParentVersion}\",
+        \"body\": \"New CoffeeNet parent version ${coffeeNetParentVersion} was released! Please check https://github.com/coffeenet/coffeenet-starter/blob/master/CHANGELOG.md for more information\",
+        \"head\": \"${organizationFrom}:${updateBranch}\",
         \"base\": \"master\",
         \"maintainer_can_modify\": true
       }"
-      echo -e " \n\n$pull_request_body\n\n"
 
-      echo "> Trying to open a pull request for $organizationFrom/$project with $updateBranch"
+      echo "> Trying to open a pull request for ${organizationFrom}/${project} with ${updateBranch}"
       pullRequestHttpStatus=$(/usr/bin/curl \
+        --location \
+        --post301 \
         --write-out '%{http_code}' \
         --silent \
         --output /dev/null \
         -X POST \
-        -H "Authorization: token $token" \
+        -H "Authorization: token ${token}" \
         -H "Accept: application/vnd.github.v3+json" \
         -H "Content-Type: application/json" \
-        "https://api.github.com/repos/$organizationTo/$project/pulls" \
+        "https://api.github.com/repos/${organizationTo}/${project}/pulls" \
         -d "$pull_request_body")
-      if [[ "$pullRequestHttpStatus" == "202" ]]; then
-        echo -e "> Opened pull request for $organizationFrom/$project on $updateBranch\n"
+      if [[ "$pullRequestHttpStatus" == "201" ]]; then
+        echo -e "    -> Opened pull request for ${organizationFrom}/${project} on ${updateBranch}\n"
       else
-        echo -e "\n> Could not open pull request for $organizationTo/$project on $updateBranch ($pullRequestHttpStatus) -> exiting"
-        exit -1
+        echo -e "  X -> Could not open pull request for ${organizationTo}/${project} on ${updateBranch} ($pullRequestHttpStatus)\n"
       fi
 
-      /usr/bin/git -C ${projectsDir}/${project} checkout master
-      /usr/bin/git -C ${projectsDir}/${project} branch -D ${updateBranch}
-      echo -e "> Deleted local branch '$updateBranch' on '$organizationFrom/$project'\n"
+      /usr/bin/git -C ${projectsDir}/${project} checkout master &> /dev/null
+      /usr/bin/git -C ${projectsDir}/${project} branch -D ${updateBranch} &> /dev/null
+      echo -e "> Deleted local branch '${updateBranch}' on '${organizationFrom}/${project}'\n"
 
     else
       echo -e ">>>> Nothing to upgrade - no new CoffeeNet parent available!\n\n"
