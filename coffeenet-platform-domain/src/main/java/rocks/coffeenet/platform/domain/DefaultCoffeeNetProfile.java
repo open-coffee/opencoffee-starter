41f5f8fedbb2f10cf3a1ac9992b1f8d925dd0e66
Florian Krupicka <krupicka@synyx.de>
2020-02-16 23:54:24 +0100

    Add CoffeeNet user core domain

---
+++ coffeenet-platform-domain/src/main/java/rocks/coffeenet/platform/domain/DefaultCoffeeNetProfile.java
@@ -0,0 +1,158 @@
+package rocks.coffeenet.platform.domain;
+
+import java.net.URL;
+
+
+/**
+ * Simple default implementation of {@link CoffeeNetProfile}. Instances are created only via a builder pattern with
+ * {@link Builder}.
+ *
+ * @author  Florian 'punycode' Krupicka - zh@punyco.de
+ */
+public class DefaultCoffeeNetProfile implements CoffeeNetProfile {
+
+    private final String uniqueIdentifier;
+    private final String name;
+
+    private String humanReadableName;
+    private URL profileURL;
+    private URL pictureURL;
+    private String email;
+
+    private DefaultCoffeeNetProfile(String uniqueIdentifier, String name) {
+
+        this.uniqueIdentifier = uniqueIdentifier;
+        this.name = name;
+    }
+
+    public static Builder withUniqueIdentifierAndName(String uniqueIdentifier, String name) {
+
+        return new Builder(uniqueIdentifier, name);
+    }
+
+
+    @Override
+    public String getUniqueIdentifier() {
+
+        return uniqueIdentifier;
+    }
+
+
+    @Override
+    public String getName() {
+
+        return name;
+    }
+
+
+    @Override
+    public String getHumanReadableName() {
+
+        return humanReadableName != null ? humanReadableName : getName();
+    }
+
+
+    void setHumanReadableName(String humanReadableName) {
+
+        this.humanReadableName = humanReadableName;
+    }
+
+
+    @Override
+    public URL getProfileURL() {
+
+        return profileURL;
+    }
+
+
+    void setProfileURL(URL profileURL) {
+
+        this.profileURL = profileURL;
+    }
+
+
+    @Override
+    public URL getPictureURL() {
+
+        return pictureURL;
+    }
+
+
+    void setPictureURL(URL pictureURL) {
+
+        this.pictureURL = pictureURL;
+    }
+
+
+    @Override
+    public String getEmail() {
+
+        return email;
+    }
+
+
+    void setEmail(String email) {
+
+        this.email = email;
+    }
+
+    public static class Builder {
+
+        private final String uniqueIdentifier;
+        private final String name;
+
+        private String humanReadableName;
+        private URL profileURL;
+        private URL pictureURL;
+        private String email;
+
+        public Builder(String uniqueIdentifier, String name) {
+
+            this.uniqueIdentifier = uniqueIdentifier;
+            this.name = name;
+        }
+
+        public Builder withHumanReadableName(String humanReadableName) {
+
+            this.humanReadableName = humanReadableName;
+
+            return this;
+        }
+
+
+        public Builder withProfileURL(URL profileURL) {
+
+            this.profileURL = profileURL;
+
+            return this;
+        }
+
+
+        public Builder withPictureURL(URL pictureURL) {
+
+            this.pictureURL = pictureURL;
+
+            return this;
+        }
+
+
+        public Builder withEmail(String email) {
+
+            this.email = email;
+
+            return this;
+        }
+
+
+        public CoffeeNetProfile build() {
+
+            DefaultCoffeeNetProfile profile = new DefaultCoffeeNetProfile(uniqueIdentifier, name);
+            profile.setHumanReadableName(humanReadableName);
+            profile.setProfileURL(profileURL);
+            profile.setPictureURL(pictureURL);
+            profile.setEmail(email);
+
+            return profile;
+        }
+    }
+}
---
+++ coffeenet-platform-domain/src/test/java/rocks/coffeenet/platform/domain/DefaultCoffeeNetProfileTests.java
@@ -0,0 +1,70 @@
+package rocks.coffeenet.platform.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * @author  Florian 'punycode' Krupicka - zh@punyco.de
+ */
+@DisplayName("DefaultCoffeeNetProfile")
+class DefaultCoffeeNetProfileTests {
+
+    @Test
+    @DisplayName("should construct a valid object with only the required arguments")
+    void simpleConstructionWithOnlyRequiredArguments() {
+
+        // Given & When
+        CoffeeNetProfile profile = DefaultCoffeeNetProfile
+                .withUniqueIdentifierAndName("the-id", "coffeenet")
+                .build();
+
+        // Then
+        assertThat(profile).extracting(CoffeeNetProfile::getUniqueIdentifier)
+            .isNotNull()
+            .isEqualTo("the-id");
+        assertThat(profile).extracting(CoffeeNetProfile::getName)
+            .isNotNull()
+            .isEqualTo("coffeenet");
+    }
+
+
+    @Test
+    @DisplayName("should construct a valid object with all arguments provided by builder")
+    void complexConstruction() throws MalformedURLException {
+
+        // Given & When
+        CoffeeNetProfile profile = DefaultCoffeeNetProfile
+                .withUniqueIdentifierAndName("the-id", "coffeenet")
+                .withHumanReadableName("CoffeeNet User")
+                .withEmail("coffeenet@example.com")
+                .withPictureURL(new URL("http://example.com/~coffeenet/me.jpg"))
+                .withProfileURL(new URL("http://example.com/~coffeenet/"))
+                .build();
+
+        // Then
+        assertThat(profile).extracting(CoffeeNetProfile::getUniqueIdentifier)
+            .isNotNull()
+            .isEqualTo("the-id");
+        assertThat(profile).extracting(CoffeeNetProfile::getName)
+            .isNotNull()
+            .isEqualTo("coffeenet");
+        assertThat(profile).extracting(CoffeeNetProfile::getHumanReadableName)
+            .isNotNull()
+            .isEqualTo("CoffeeNet User");
+        assertThat(profile).extracting(CoffeeNetProfile::getEmail)
+            .isNotNull()
+            .isEqualTo("coffeenet@example.com");
+        assertThat(profile).extracting(CoffeeNetProfile::getPictureURL)
+            .isNotNull()
+            .isEqualTo(new URL("http://example.com/~coffeenet/me.jpg"));
+        assertThat(profile).extracting(CoffeeNetProfile::getProfileURL)
+            .isNotNull()
+            .isEqualTo(new URL("http://example.com/~coffeenet/"));
+    }
+}
