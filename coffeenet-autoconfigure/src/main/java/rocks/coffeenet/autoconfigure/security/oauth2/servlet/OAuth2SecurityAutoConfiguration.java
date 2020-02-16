8a03c0222e32234e8b254a10232cfc811a775322
Florian Krupicka <krupicka@synyx.de>
2020-02-16 22:35:50 +0100

    Auto-enable OAuth2 security if clients are configured
    This makes use of the feature from the previous commit, to automatically
    configure features on instances of `WebSecurityConfigurerAdapter`, if
    certain conditions are met. In this case, we auto-enable OAuth2.

---
+++ coffeenet-autoconfigure/src/main/java/rocks/coffeenet/autoconfigure/security/oauth2/servlet/OAuth2SecurityAutoConfiguration.java
@@ -0,0 +1,41 @@
+package rocks.coffeenet.autoconfigure.security.oauth2.servlet;
+
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.ClientsConfiguredCondition;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+
+
+/**
+ * {@link EnableAutoConfiguration Auto-configuration} for Spring Security OAuth2 in CoffeeNet applications.
+ *
+ * @author  Florian 'punycode' Krupicka - zh@punyco.de
+ */
+@Configuration
+@AutoConfigureBefore(SecurityAutoConfiguration.class)
+@ConditionalOnClass({ EnableWebSecurity.class, ClientRegistration.class })
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+@Conditional(ClientsConfiguredCondition.class)
+public class OAuth2SecurityAutoConfiguration {
+
+    @Configuration
+    @ConditionalOnMissingBean(OAuth2SecurityConfigurer.class)
+    static class OAuth2SecurityConfigurerConfiguration {
+
+        @Bean
+        public OAuth2SecurityConfigurer coffeeNetOAuth2SecurityConfigurer() {
+
+            return new OAuth2SecurityConfigurer();
+        }
+    }
+}
