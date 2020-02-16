e80361a9bb456c3429c3c7828f4d6db2136894ac
Florian Krupicka <krupicka@synyx.de>
2020-02-16 20:15:47 +0100

    Add ability to hook bean-defined security configurers globally
    To enable more CoffeeNet autoconfiguration possibilities, we need to
    hook into the process of defining global `SecurityConfigurer` beans,
    that are applied to instances of `WebSecurityConfigurerAdapter`.

---
+++ coffeenet-autoconfigure/src/main/java/rocks/coffeenet/autoconfigure/security/CoffeeNetSecurityAutoConfiguration.java
@@ -0,0 +1,22 @@
+package rocks.coffeenet.autoconfigure.security;
+
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+
+
+/**
+ * {@link EnableAutoConfiguration Auto-configuration} for Spring Security in CoffeeNet applications.
+ *
+ * @author  Florian 'punycode' Krupicka - zh@punyco.de
+ */
+@Configuration
+@ConditionalOnClass(DefaultAuthenticationEventPublisher.class)
+@AutoConfigureBefore(SecurityAutoConfiguration.class)
+public class CoffeeNetSecurityAutoConfiguration {
+}
