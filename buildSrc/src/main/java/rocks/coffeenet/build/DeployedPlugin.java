97be5d8294923b49219c82bf7f1d15ebbddea578
Florian Krupicka <krupicka@synyx.de>
2020-03-29 17:09:47 +0200

    buildSrc: Enable SonaType OSS repository publishing support
    This enables the Nexus Publish Plugin and the Nexus Staging Plugin for
    publishing the generated artifacts to the SonaType OSS repository.

--- buildSrc/src/main/java/rocks/coffeenet/build/DeployedPlugin.java
+++ buildSrc/src/main/java/rocks/coffeenet/build/DeployedPlugin.java
@@ -1,5 +1,10 @@
 package rocks.coffeenet.build;
 
+import de.marcphilipp.gradle.nexus.NexusPublishExtension;
+import de.marcphilipp.gradle.nexus.NexusPublishPlugin;
+
+import io.codearte.gradle.nexus.NexusStagingPlugin;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPlatformPlugin;
@@ -10,6 +15,7 @@ import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
 
 
 // This is taken from https://github.com/spring-projects/spring-boot/blob/master/buildSrc/src/main/java/org/springframework/boot/build/DeployedPlugin.java
+
 /**
  * A plugin applied to a project that should be deployed.
  *
@@ -21,6 +27,11 @@ public class DeployedPlugin implements Plugin<Project> {
      * Name of the task that generates the deployed pom file.
      */
     public static final String GENERATE_POM_TASK_NAME = "generatePomFileForMavenPublication";
+
+    /**
+     * Property to determine if to apply publication to SonaType OSS repository.
+     */
+    public static final String SONATYPE_OSS_PROPERTY_NAME = "sonatype";
 
     @Override
     public void apply(Project project) {
@@ -42,7 +53,28 @@ public class DeployedPlugin implements Plugin<Project> {
         project.getPlugins().withType(JavaPlatformPlugin.class).all((plugin) ->
             project.getComponents().matching((component) -> component.getName().equals("javaPlatform"))
                 .all(mavenPublication::from));
+
+        if (project.hasProperty(SONATYPE_OSS_PROPERTY_NAME)) {
+            applySonaTypeOssPublishing(project);
+        }
+
         //@formatter:on
         //J+
+    }
+
+
+    /**
+     * Apply the publishing extensions for SonaType OSS repository.
+     */
+    private void applySonaTypeOssPublishing(Project project) {
+
+        Project rootProject = project.getRootProject();
+
+        if (!project.getRootProject().getPlugins().hasPlugin(NexusStagingPlugin.class)) {
+            rootProject.getPlugins().apply(NexusStagingPlugin.class);
+        }
+
+        project.getPlugins().apply(NexusPublishPlugin.class);
+        project.getExtensions().getByType(NexusPublishExtension.class).getRepositories().sonatype();
     }
 }
